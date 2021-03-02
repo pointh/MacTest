04f23ee82ea584e97aa9a54b121acf6982510f0b
pointh <pavel.svec@gmail.com>
2021-03-02 09:35:20 +0100

    Zkouška

---
+++ MacTest/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MacTest.Services;
+using MacTest.Views;
+
+namespace MacTest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MacTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MacTest.Models;
+
+namespace MacTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
